46b6c4fe77cf02b7e842b41d14fac53f1c072649
KevinGuedes <kevinguedes1@gmail.com>
2023-06-12 21:18:26 -0300

    style: fix identation

--- src/components/ContentSection.vue
+++ src/components/ContentSection.vue
@@ -3,6 +3,8 @@
     <h3>
       <slot name="title"> <h3>Fallback Title</h3> </slot>
     </h3>
-    <slot> <p>Default Fallback Content</p> </slot>
+    <slot>
+      <p>Default Fallback Content</p>
+    </slot>
   </section>
 </template>
